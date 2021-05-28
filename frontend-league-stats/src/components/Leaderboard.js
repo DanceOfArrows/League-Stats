04d4cbcfd2c6bfba4ad74dec0a0aad44521b1e6b
Seamus Le <seamust.le@gmail.com>
2021-05-27 23:11:24 -0700

    added leaderboard.  created layout.  TODO: map each player and pagination

--- frontend-league-stats/src/components/Leaderboard.js
+++ frontend-league-stats/src/components/Leaderboard.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { connect } from "react-redux";
 
 import { getLeaderboard } from "../redux/leaderboard";
+import Loader from "./Loader";
 
 const Leaderboard = (props) => {
   const { leaderboard, getLeaderboard } = props;
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     if (!leaderboard) getLeaderboard();
@@ -14,7 +17,31 @@ const Leaderboard = (props) => {
 
   return (
     <>
-      <div className="league-stats-test-long-div"></div>
+      <div className="league-stats-leaderboard page-container">
+        {leaderboard ? (
+          <>
+            <div className="league-stats-leaderboard-box">
+              <h2 style={{ gridColumn: "1 / 2" }}>#</h2>
+              <h2 style={{ gridColumn: "2 / 3" }}>Name</h2>
+              <h2 style={{ gridColumn: "3 / 4" }}>LP</h2>
+              <h2 style={{ gridColumn: "4 / 5" }}>Wins (Win rate)</h2>
+              <div className="league-stats-leaderboard-pagination">
+                {" "}
+                <ReactPaginate
+                  pageCount={30}
+                  pageRangeDisplayed={4}
+                  marginPagesDisplayed={1}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <Loader full="true" size="5rem" />
+        )}
+      </div>
     </>
   );
 };
